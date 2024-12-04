7e573db1ababdc5b308846cb2c55bb17f5de659e
Kasuadana Adams <sulemanaadams375@gmail.com>
2024-12-04 07:13:06 +0000

    removed components in App.js for starter code

--- ctvet-performance-optimization/src/App.js
+++ ctvet-performance-optimization/src/App.js
@@ -1,12 +1,10 @@
-import React, { Suspense, lazy, useState } from 'react';
+import React,{useState} from 'react';
 import './index.css';
 import "./App.css"
 import Navbar from './components/Navbar';
-import VirtualizedPhotos from './components/PhotosVirtualizedList';
 
-const Posts = lazy(() => import('./components/Posts'));
-const Users = lazy(() => import('./components/Users'));
-const Photos = lazy(() => import('./components/Photos'));
+
+
 
 export default function App() {
   const [page, setPage] = useState('posts');
@@ -14,12 +12,7 @@ export default function App() {
   return (
     <div className="container">
       <Navbar setPage={setPage} />
-      <Suspense fallback={<div className="loading">Loading...</div>}>
-        {page === 'posts' && <Posts />}
-        {page === 'users' && <Users />}
-        {page === 'photos' && <Photos />}
-        {page === 'list' && <VirtualizedPhotos />}
-      </Suspense>
+  
     </div>
   );
 }
